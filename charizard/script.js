b02cc682882f81ee464b447a0cdfd62a7a75b325
Jadyn Calhoun <jaycal2013@gmail.com>
2021-04-16 14:00:06 -0700

    caught up making everything through js, all current buttons active except start button

--- charizard/script.js
+++ charizard/script.js
@@ -151,57 +151,181 @@ function createScreen(obj) {
   BASICINFO.appendChild(BASICINFO4);
 
   ///making tags inside of bottom section 
+
+  // const TABS = document.createElement('div');
+  // for (let i = 0; i <= 5; i++) {
+  //   const TABSCONTAINER = document.createElement('div');
+  //   TABSCONTAINER.setAttribute('id', 'tab-3-container');
+
+  //   const TABS = document.createElement('div');
+  //   TABS.setAttribute('class', 'tabs-3');
+
+  //   TABS.setAttribute('id', `stats-${i + 1}`);
+  //   const { stats } = obj;
+
+  //   let content = `${stats[i]['stat']['name']}  >`;
+
+
+  //   const TABTITLE = document.createElement('div');
+  //   TABTITLE.setAttribute('id', 'tabTitle');
+  //   TABTITLE.textContent = content;
+
+  //   TABS.appendChild(TABTITLE);
+  //   TABSCONTAINER.appendChild(TABS);
+  //   SECONDTABSHOLDER.appendChild(TABSCONTAINER);
+  // }
+
+
   const TABSHOLDER = document.createElement("div");
   TABSHOLDER.setAttribute('class', 'tabs-1');
 
   const SECONDTABSHOLDER = document.createElement('div');
   SECONDTABSHOLDER.setAttribute('id', 'tabs-2');
-  TABSHOLDER.appendChild(SECONDTABSHOLDER);
-  // const TABS = document.createElement('div');
-  for (let i = 0; i <= 5; i++) {
-    const TABSCONTAINER = document.createElement('div');
-    TABSCONTAINER.setAttribute('id', 'tab-3-container');
-
-    const TABS = document.createElement('div');
-    TABS.setAttribute('class', 'tabs-3');
-
-    TABS.setAttribute('id', `stats-${i + 1}`);
-    const { stats } = obj;
-
-    let content = `${stats[i]['stat']['name']}  >`;
-
-
-    const TABTITLE = document.createElement('div');
-    TABTITLE.setAttribute('id', 'tabTitle');
-    TABTITLE.textContent = content;
-
-    TABS.appendChild(TABTITLE);
-    TABSCONTAINER.appendChild(TABS);
-    SECONDTABSHOLDER.appendChild(TABSCONTAINER);
-  }
-
 
   const STATSBOX = document.createElement('div');
   STATSBOX.setAttribute('id', 'statsBox');
 
   const STATNAME = document.createElement('div');
   STATNAME.setAttribute('id', 'statName');
+  STATNAME.TEXTCONTENT = 'hp';
   const BASESTAT = document.createElement('div');
   BASESTAT.setAttribute('id', 'baseStat');
+  BASESTAT.TEXTCONTENT = 'BASE STAT: 78';
+
   const EFFORT = document.createElement('div');
   EFFORT.setAttribute('id', 'effort');
+  EFFORT.TEXTCONTENT = 'EFFORT: 0';
 
-  ///needs to be able to update, this is just boilerplate for now
-  STATNAME.textContent = 'HP';
-  BASESTAT.textContent = 'BASE STAT: 78';
-  EFFORT.textContent = 'EFFORT: 0';
+  const {stats} = obj;
+  const TABSCONTAINER1 = document.createElement('div');
+  TABSCONTAINER1.setAttribute('id', 'tab-3-container');
+  const TAB1 = document.createElement('div');
+  TAB1.setAttribute('class', 'tabs-3');
+  TAB1.addEventListener('click', tab1Run);
+  window.addEventListener('load', tab1Run);
+  function tab1Run(){
 
-  STATSBOX.appendChild(STATNAME);
-  STATSBOX.appendChild(BASESTAT);
-  STATSBOX.appendChild(EFFORT);
+    STATNAME.textContent = `${stats[0]['stat']['name']}`;
+    BASESTAT.textContent = `BASE STAT: ${stats[0]['base_stat']}`;
+    EFFORT.textContent  =`EFFORT: ${stats[0]['effort']}`;
+    STATSBOX.appendChild(STATNAME);
+    STATSBOX.appendChild(BASESTAT);
+    STATSBOX.appendChild(EFFORT);
+  }
+  const TABTITLE1 = document.createElement('div');
+  TABTITLE1.setAttribute('id','tabTitle');
+  TABTITLE1.textContent = `${stats[0]['stat']['name']} >`
+  TAB1.appendChild(TABTITLE1);
+  TABSCONTAINER1.appendChild(TAB1);
+  SECONDTABSHOLDER.appendChild(TABSCONTAINER1);
 
 
+  const TABSCONTAINER2 = document.createElement('div');
+  TABSCONTAINER2.setAttribute('id', 'tab-3-container');
+  const TAB2 = document.createElement('div');
+  TAB2.setAttribute('class', 'tabs-3');
+  TAB2.addEventListener('click', tab2Run)
+  function tab2Run(){
 
+    STATNAME.textContent = `${stats[1]['stat']['name']}`;
+    BASESTAT.textContent = `BASE STAT: ${stats[1]['base_stat']}`;
+    EFFORT.textContent  =`EFFORT: ${stats[1]['effort']}`;
+    STATSBOX.appendChild(STATNAME);
+    STATSBOX.appendChild(BASESTAT);
+    STATSBOX.appendChild(EFFORT);
+  }
+  const TABTITLE2 = document.createElement('div');
+  TABTITLE2.setAttribute('id','tabTitle');
+  TABTITLE2.textContent = `${stats[1]['stat']['name']} >`
+  TAB2.appendChild(TABTITLE2);
+  TABSCONTAINER2.appendChild(TAB2);
+  SECONDTABSHOLDER.appendChild(TABSCONTAINER2);
+
+  const TABSCONTAINER3 = document.createElement('div');
+  TABSCONTAINER3.setAttribute('id', 'tab-3-container');
+  const TAB3 = document.createElement('div');
+  TAB3.setAttribute('class', 'tabs-3');
+  TAB3.addEventListener('click', tab3Run)
+  function tab3Run(){
+
+    STATNAME.textContent = `${stats[2]['stat']['name']}`;
+    BASESTAT.textContent = `BASE STAT: ${stats[2]['base_stat']}`;
+    EFFORT.textContent  =`EFFORT: ${stats[2]['effort']}`;
+    STATSBOX.appendChild(STATNAME);
+    STATSBOX.appendChild(BASESTAT);
+    STATSBOX.appendChild(EFFORT);
+  }
+  const TABTITLE3 = document.createElement('div');
+  TABTITLE3.setAttribute('id','tabTitle');
+  TABTITLE3.textContent = `${stats[2]['stat']['name']} >`
+  TAB3.appendChild(TABTITLE3);
+  TABSCONTAINER3.appendChild(TAB3);
+  SECONDTABSHOLDER.appendChild(TABSCONTAINER3);
+
+  const TABSCONTAINER4 = document.createElement('div');
+  TABSCONTAINER4.setAttribute('id', 'tab-3-container');
+  const TAB4 = document.createElement('div');
+  TAB4.setAttribute('class', 'tabs-3');
+  TAB4.addEventListener('click', tab4Run)
+  function tab4Run(){
+
+    STATNAME.textContent = `${stats[3]['stat']['name']}`;
+    BASESTAT.textContent = `BASE STAT: ${stats[3]['base_stat']}`;
+    EFFORT.textContent  =`EFFORT: ${stats[3]['effort']}`;
+    STATSBOX.appendChild(STATNAME);
+    STATSBOX.appendChild(BASESTAT);
+    STATSBOX.appendChild(EFFORT);
+  }
+  const TABTITLE4 = document.createElement('div');
+  TABTITLE4.setAttribute('id','tabTitle');
+  TABTITLE4.textContent = `${stats[3]['stat']['name']} >`
+  TAB4.appendChild(TABTITLE4);
+  TABSCONTAINER4.appendChild(TAB4);
+  SECONDTABSHOLDER.appendChild(TABSCONTAINER4);
+
+  const TABSCONTAINER5 = document.createElement('div');
+  TABSCONTAINER5.setAttribute('id', 'tab-3-container');
+  const TAB5 = document.createElement('div');
+  TAB5.setAttribute('class', 'tabs-3');
+  TAB5.addEventListener('click', tab5Run)
+  function tab5Run(){
+
+    STATNAME.textContent = `${stats[4]['stat']['name']}`;
+    BASESTAT.textContent = `BASE STAT: ${stats[4]['base_stat']}`;
+    EFFORT.textContent  =`EFFORT: ${stats[4]['effort']}`;
+    STATSBOX.appendChild(STATNAME);
+    STATSBOX.appendChild(BASESTAT);
+    STATSBOX.appendChild(EFFORT);
+  }
+  const TABTITLE5 = document.createElement('div');
+  TABTITLE5.setAttribute('id','tabTitle');
+  TABTITLE5.textContent = `${stats[4]['stat']['name']} >`
+  TAB5.appendChild(TABTITLE5);
+  TABSCONTAINER5.appendChild(TAB5);
+  SECONDTABSHOLDER.appendChild(TABSCONTAINER5);
+
+  const TABSCONTAINER6 = document.createElement('div');
+  TABSCONTAINER6.setAttribute('id', 'tab-3-container');
+  const TAB6 = document.createElement('div');
+  TAB6.setAttribute('class', 'tabs-3');
+  TAB6.addEventListener('click', tab6Run)
+  function tab6Run(){
+
+    STATNAME.textContent = `${stats[5]['stat']['name']}`;
+    BASESTAT.textContent = `BASE STAT: ${stats[5]['base_stat']}`;
+    EFFORT.textContent  =`EFFORT: ${stats[5]['effort']}`;
+    STATSBOX.appendChild(STATNAME);
+    STATSBOX.appendChild(BASESTAT);
+    STATSBOX.appendChild(EFFORT);
+  }
+  const TABTITLE6 = document.createElement('div');
+  TABTITLE6.setAttribute('id','tabTitle');
+  TABTITLE6.textContent = `${stats[5]['stat']['name']} >`
+  TAB6.appendChild(TABTITLE6);
+  TABSCONTAINER6.appendChild(TAB6);
+  SECONDTABSHOLDER.appendChild(TABSCONTAINER6);
+
+  TABSHOLDER.appendChild(SECONDTABSHOLDER);
 
 
 
